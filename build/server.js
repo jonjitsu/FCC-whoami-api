a4b659b312148ec878f2297ee0a17365ebf39730
Jonathan Willis <jonathanwillis@linux.com>
2016-01-17 23:32:02 -0500

    Added build + pre-heroku initial test

---
+++ build/server.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const PORT = process.env.PORT || 3000;
+
+var assert = require('assert'),
+    express = require('express'),
+    app = express().get('/', function (req, res) {
+    res.send('It Works JW!');
+})
+// .get('/:value', function(req, res) {
+//     res.json(ts.response(ts.parseValue(req.params.value)));
+// })
+.use(function (req, res) {
+    res.sendStatus(404);
+}),
+    server = app.listen(PORT, function () {
+    let port = server.address().port;
+    console.log('Up an runnin');
+});
